19218408279a350d24d463e4e8d43d15a961d29d
胡日千 <jhlxhrq@163.com>
2022-06-09 16:01:57 +0800

    完成了蚕室环境数据（随机数）注入

--- src/main/java/fun/riqian/controller/environment/EnvironmentController.java
+++ src/main/java/fun/riqian/controller/environment/EnvironmentController.java
@@ -4,7 +4,9 @@ import fun.riqian.domain.Environment;
 import fun.riqian.service.environment.EnvironmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -22,10 +24,17 @@ public class EnvironmentController {
     /**
      * @return 返回小程序所需要的 JSON 数据
      */
-    @RequestMapping("/findAll")
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     // @RequestBody可以获取请求体，需要在控制器方法设置一个形参，使用@RequestBody进行标识，
     // 当前请求的请求体就会为当前注解所标识的形参赋值
     public @ResponseBody List<Environment> findAll() {
         return envSrv.findAll();
+    }
+
+    @RequestMapping(value = "/envList", method = RequestMethod.GET)
+    public String getEnvList(Model model) {
+        List<Environment> list = envSrv.findAll();
+        model.addAttribute("envList", list);
+        return "environment/environment_list";
     }
 }
